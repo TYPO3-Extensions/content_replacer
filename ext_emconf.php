c96f030bdac118825b5dd52e8224686b6032f360
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-08 12:06:40 +0000

    released version 1.2.4
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/content_replacer/trunk@21132 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "content_replacer"
 #
-# Auto generated 28-04-2009 15:49
+# Auto generated 08-06-2009 14:06
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This extension parses your content and replaces keywords in special span tags with a defined replacement. It supports workspaces, multilanguage, categories, wildcard terms, RTE insertion and stdWrap properties on the replacement text.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.2.3',
+	'version' => '1.2.4',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:18:{s:29:"class.tx_content_replacer.php";s:4:"fbd4";s:19:"de.locallang_db.xml";s:4:"5314";s:21:"ext_conf_template.txt";s:4:"2acc";s:12:"ext_icon.gif";s:4:"8fdb";s:17:"ext_localconf.php";s:4:"55d8";s:14:"ext_tables.php";s:4:"e345";s:14:"ext_tables.sql";s:4:"709b";s:16:"locallang_db.xml";s:4:"7543";s:7:"tca.php";s:4:"c9c0";s:53:"resources/icons/icon_tx_content_replacer_category.png";s:4:"3338";s:56:"resources/icons/icon_tx_content_replacer_category__h.png";s:4:"8e19";s:56:"resources/icons/icon_tx_content_replacer_category__t.png";s:4:"8e19";s:49:"resources/icons/icon_tx_content_replacer_term.png";s:4:"6983";s:52:"resources/icons/icon_tx_content_replacer_term__h.png";s:4:"ca94";s:52:"resources/icons/icon_tx_content_replacer_term__t.png";s:4:"ca94";s:14:"doc/manual.sxw";s:4:"eb99";s:20:"static/constants.txt";s:4:"dd40";s:16:"static/setup.txt";s:4:"ef8f";}',
+	'_md5_values_when_last_written' => 'a:18:{s:29:"class.tx_content_replacer.php";s:4:"bfa9";s:19:"de.locallang_db.xml";s:4:"5314";s:21:"ext_conf_template.txt";s:4:"2acc";s:12:"ext_icon.gif";s:4:"8fdb";s:17:"ext_localconf.php";s:4:"1638";s:14:"ext_tables.php";s:4:"e345";s:14:"ext_tables.sql";s:4:"709b";s:16:"locallang_db.xml";s:4:"7543";s:7:"tca.php";s:4:"c9c0";s:53:"resources/icons/icon_tx_content_replacer_category.png";s:4:"3338";s:56:"resources/icons/icon_tx_content_replacer_category__h.png";s:4:"8e19";s:56:"resources/icons/icon_tx_content_replacer_category__t.png";s:4:"8e19";s:49:"resources/icons/icon_tx_content_replacer_term.png";s:4:"6983";s:52:"resources/icons/icon_tx_content_replacer_term__h.png";s:4:"ca94";s:52:"resources/icons/icon_tx_content_replacer_term__t.png";s:4:"ca94";s:14:"doc/manual.sxw";s:4:"eb99";s:20:"static/constants.txt";s:4:"dd40";s:16:"static/setup.txt";s:4:"ef8f";}',
 	'suggests' => array(
 	),
 );
