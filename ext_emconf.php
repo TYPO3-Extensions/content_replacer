a6deea0a75d409c851609cb1512043adae874b96
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-30 19:31:31 +0000

    changed description and added requirements/suggestions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/content_replacer/trunk@26036 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Content Replacer',
-	'description' => 'This extension parses your content and replaces keywords in special span tags with a defined replacement. It supports workspaces, multilanguage, categories, wildcard terms, RTE insertion and stdWrap properties on the replacement text.',
+	'description' => 'This extension parses your content and replaces keywords in special span tags with a defined replacement. It supports multilanguage, categories, wildcard terms, RTE insertion and stdWrap properties on the replacement text.',
 	'category' => 'fe',
 	'shy' => 0,
 	'version' => '1.3.2',
@@ -34,10 +34,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.0.0-4.2.99',
+            'php' => '5.0.0-5.2.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
+			'tinymce_rte' => '-'
 		),
 	),
 	'_md5_values_when_last_written' => 'a:18:{s:29:"class.tx_content_replacer.php";s:4:"db73";s:19:"de.locallang_db.xml";s:4:"5314";s:21:"ext_conf_template.txt";s:4:"6320";s:12:"ext_icon.gif";s:4:"8fdb";s:17:"ext_localconf.php";s:4:"c204";s:14:"ext_tables.php";s:4:"c5eb";s:14:"ext_tables.sql";s:4:"07c8";s:16:"locallang_db.xml";s:4:"7543";s:7:"tca.php";s:4:"d19b";s:53:"resources/icons/icon_tx_content_replacer_category.png";s:4:"dd7a";s:56:"resources/icons/icon_tx_content_replacer_category__h.png";s:4:"ba3c";s:56:"resources/icons/icon_tx_content_replacer_category__t.png";s:4:"dbe8";s:49:"resources/icons/icon_tx_content_replacer_term.png";s:4:"448e";s:52:"resources/icons/icon_tx_content_replacer_term__h.png";s:4:"20f0";s:52:"resources/icons/icon_tx_content_replacer_term__t.png";s:4:"0f21";s:14:"doc/manual.sxw";s:4:"f3eb";s:20:"static/constants.txt";s:4:"dd40";s:16:"static/setup.txt";s:4:"ef8f";}',
