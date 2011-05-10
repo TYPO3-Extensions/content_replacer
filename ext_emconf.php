67c0ef5e438c16e09282f6f97457370ca4a30f7e
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-10 22:48:54 +0000

    [TASK] Remove tinymce_rte suggestion and rte directory requirement [TASK] Adjust the TYPO3 and PHP version requirements as older versions are not tested
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/content_replacer/trunk@47555 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'module' => '',
 	'state' => 'stable',
 	'uploadfolder' => 0,
-	'createDirs' => 'uploads/tx_content_parser/rte',
+	'createDirs' => '',
 	'modify_tables' => '',
 	'clearcacheonload' => 0,
 	'lockType' => '',
@@ -34,13 +34,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.0.0-4.4.99',
-			'php' => '5.0.0-5.3.99',
+			'typo3' => '4.5.0-4.5.99',
+			'php' => '5.2.0-5.3.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'tinymce_rte' => '-',
 		),
 	),
 	'_md5_values_when_last_written' => 'a:18:{s:19:"de.locallang_db.xml";s:4:"1c99";s:21:"ext_conf_template.txt";s:4:"6320";s:12:"ext_icon.gif";s:4:"8fdb";s:17:"ext_localconf.php";s:4:"4086";s:14:"ext_tables.php";s:4:"c5eb";s:14:"ext_tables.sql";s:4:"07c8";s:16:"locallang_db.xml";s:4:"cb24";s:7:"tca.php";s:4:"1b6d";s:37:"classes/class.tx_content_replacer.php";s:4:"b371";s:14:"doc/manual.sxw";s:4:"2bd8";s:53:"resources/icons/icon_tx_content_replacer_category.png";s:4:"dd7a";s:56:"resources/icons/icon_tx_content_replacer_category__h.png";s:4:"ba3c";s:56:"resources/icons/icon_tx_content_replacer_category__t.png";s:4:"dbe8";s:49:"resources/icons/icon_tx_content_replacer_term.png";s:4:"448e";s:52:"resources/icons/icon_tx_content_replacer_term__h.png";s:4:"20f0";s:52:"resources/icons/icon_tx_content_replacer_term__t.png";s:4:"0f21";s:20:"static/constants.txt";s:4:"5173";s:16:"static/setup.txt";s:4:"f383";}',
