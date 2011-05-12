006ac290f0fa00670c3af8fa352d61a18da335ff
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-12 22:40:10 +0000

    [TASK] Release of version 1.5.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/content_replacer/trunk@47668 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "content_replacer".
 #
-# Auto generated 19-08-2010 21:55
+# Auto generated 13-05-2011 00:39
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'You need a fast substitution of terms with full support of typoscript, categories and RTE integration? If yes, the extension could be perfectly fit into your project. The performance is gained by some manual work in the content by wrapping the replacement terms.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.4.0',
+	'version' => '1.5.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:18:{s:19:"de.locallang_db.xml";s:4:"1c99";s:21:"ext_conf_template.txt";s:4:"6320";s:12:"ext_icon.gif";s:4:"8fdb";s:17:"ext_localconf.php";s:4:"4086";s:14:"ext_tables.php";s:4:"c5eb";s:14:"ext_tables.sql";s:4:"07c8";s:16:"locallang_db.xml";s:4:"cb24";s:7:"tca.php";s:4:"1b6d";s:37:"classes/class.tx_content_replacer.php";s:4:"b371";s:14:"doc/manual.sxw";s:4:"2bd8";s:53:"resources/icons/icon_tx_content_replacer_category.png";s:4:"dd7a";s:56:"resources/icons/icon_tx_content_replacer_category__h.png";s:4:"ba3c";s:56:"resources/icons/icon_tx_content_replacer_category__t.png";s:4:"dbe8";s:49:"resources/icons/icon_tx_content_replacer_term.png";s:4:"448e";s:52:"resources/icons/icon_tx_content_replacer_term__h.png";s:4:"20f0";s:52:"resources/icons/icon_tx_content_replacer_term__t.png";s:4:"0f21";s:20:"static/constants.txt";s:4:"5173";s:16:"static/setup.txt";s:4:"f383";}',
+	'_md5_values_when_last_written' => 'a:23:{s:19:"de.locallang_db.xml";s:4:"1c99";s:16:"ext_autoload.php";s:4:"436a";s:21:"ext_conf_template.txt";s:4:"eb18";s:12:"ext_icon.gif";s:4:"8fdb";s:17:"ext_localconf.php";s:4:"6b07";s:14:"ext_tables.php";s:4:"c0aa";s:14:"ext_tables.sql";s:4:"c858";s:16:"locallang_db.xml";s:4:"cb24";s:7:"tca.php";s:4:"1e47";s:63:"Classes/Controller/class.tx_contentreplacer_controller_main.php";s:4:"67c3";s:63:"Classes/Repository/class.tx_contentreplacer_repository_term.php";s:4:"c56d";s:67:"Classes/Service/class.tx_contentreplacer_service_abstractparser.php";s:4:"f5f2";s:65:"Classes/Service/class.tx_contentreplacer_service_customparser.php";s:4:"ea92";s:63:"Classes/Service/class.tx_contentreplacer_service_spanparser.php";s:4:"559c";s:14:"doc/manual.sxw";s:4:"55b1";s:53:"resources/icons/icon_tx_content_replacer_category.png";s:4:"dd7a";s:56:"resources/icons/icon_tx_content_replacer_category__h.png";s:4:"ba3c";s:56:"resources/icons/icon_tx_content_replacer_category__t.png";s:4:"dbe8";s:49:"resources/icons/icon_tx_content_replacer_term.png";s:4:"448e";s:52:"resources/icons/icon_tx_content_replacer_term__h.png";s:4:"20f0";s:52:"resources/icons/icon_tx_content_replacer_term__t.png";s:4:"0f21";s:20:"static/constants.txt";s:4:"600a";s:16:"static/setup.txt";s:4:"d12c";}',
 	'suggests' => array(
 	),
 );
